ac2684f37f07795de7ad15cca4dada0bde925466
IgorMushk <igor.mushketik@gmail.com>
2023-10-05 14:15:35 +0300

    code refactoring - createReducer (reducer.js) - del  comments
    replase tasksInitialSta on filtersInitialState in filtersReducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -17,23 +17,6 @@ const tasksInitialState = [
 
 // Отвечает только за обновление свойства tasks
 // Теперь значением параметра state будет массив задач
-// export const tasksReducer = (state = tasksInitialState, action) => {
-//     switch (action.type) {
-//       case addTask.type:
-//         return [...state, action.payload];
-//       case deleteTask.type:
-//         return state.filter(task => task.id !== action.payload);
-//       case toggleCompleted.type:
-//         return state.map(task => {
-//           if (task.id !== action.payload) {
-//             return task;
-//           }
-//           return { ...task, completed: !task.completed };
-//         });
-//       default:
-//         return state;
-//     }
-//   };
 export const tasksReducer = createReducer(tasksInitialState, {
   [addTask]: (state, action) => {
     return [...state, action.payload];
@@ -57,18 +40,7 @@ const filtersInitialState = {
 
 // Отвечает только за обновление свойства filters
 // Теперь значением параметра state будет объект фильтров
-// export const filtersReducer = (state = filtersInitialState, action) => {
-//   switch (action.type) {
-//     case setStatusFilter.type:
-//       return {
-//         ...state,
-//         status: action.payload,
-//       };
-//     default:
-//       return state;
-//   }
-// };
-export const filtersReducer = createReducer(tasksInitialState, {
+export const filtersReducer = createReducer(filtersInitialState , {
  [setStatusFilter]: (state, action) =>{
       return {
         ...state,
@@ -76,13 +48,3 @@ export const filtersReducer = createReducer(tasksInitialState, {
       };
  }
 });
-
-// // Код редюсеров tasksReducer и filtersReducer
-// export const rootReducer = (state = {}, action) => {
-//   // Возвращаем объект состояния
-//   return {
-//     // Обоим редюсерам передаем только часть состояния за которую они отвечают
-//     tasks: tasksReducer(state.tasks, action),
-//     filters: filtersReducer(state.filters, action),
-//   };
-// };
